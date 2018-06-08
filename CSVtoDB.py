1f1a0e7e94d8a2bee2248196458945e54daee4be
puhuk <wetr235@gmail.com>
2018-06-08 23:08:20 +0900

    Add files via upload

---
+++ CSVtoDB.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#Python 2.7 버전 사용
+import csv
+import sys
+
+filename = 'Stock_list.csv'
+f = open(filename, 'rb')
+reader = csv.reader(f)
+try:
+    for row in reader:
+        for r in row:
+            print r.decode('euckr').encode('utf-8')
+
+except csv.Error as e:
+    sys.exit('file %s, line %d: %s \n' % (filename, reader.line_num, e))
+f.close()
